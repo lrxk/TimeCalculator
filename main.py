58bf9421b7cb99b63107699629f62db7a2468b38
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-09-21 06:55:10 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import datetime
-user_time_start=input("Enter a starting hour and minute time like the following : 05:22")
-user_time_end=input("Enter a ending hour and minute time like the following : 06:19")
-datetime_user_time_start=datetime.datetime.strptime(user_time_start,"HH:MM")
-datetime_user_time_end=datetime.datetime.strptime(user_time_end,"HH:MM")
-while datetime_user_time_start>datetime_user_time_end:
-    print("Error: Starting time must be inferior to ending time")
+def input_manager():
     user_time_start=input("Enter a starting hour and minute time like the following : 05:22")
     user_time_end=input("Enter a ending hour and minute time like the following : 06:19")
     datetime_user_time_start=datetime.datetime.strptime(user_time_start,"HH:MM")
     datetime_user_time_end=datetime.datetime.strptime(user_time_end,"HH:MM")
+    return datetime_user_time_start,datetime_user_time_end
+datetime_user_time_start,datetime_user_time_end=input_manager()
+while datetime_user_time_start>datetime_user_time_end:
+    print("Error: Starting time must be inferior to ending time")
+    datetime_user_time_start,datetime_user_time_end=input_manager()
+
+
 
